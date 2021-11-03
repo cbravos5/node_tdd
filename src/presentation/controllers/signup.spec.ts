bd0f54dc4ce4475ba1a45444d9dc7a69d9df27cc
cbravos5 <cnbravos5@gmail.com>
2021-11-03 15:52:39 -0300

    feat: all

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no nome is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password_confirmation'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+})
