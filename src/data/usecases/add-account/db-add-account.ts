8755c4a5a324eee6d5d864c6b2288b41aed704d6
cbravos5 <cnbravos5@gmail.com>
2022-01-10 11:46:12 -0300

    refactor: group all protocols in a single file

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,16 +1,14 @@
-import { AccountModel } from '../../../domain/models/account'
-import { AddAccount, AddAccountModel } from '../../../domain/usecases/add-account'
-import { Encrypter } from '../../protocols/encrypter'
+import { AccountModel, AddAccount, AddAccountModel, Encrypter } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
-  private readonly encrypter
+  private readonly encrypter: Encrypter
 
   constructor (encrypter: Encrypter) {
     this.encrypter = encrypter
   }
 
   async add (account: AddAccountModel): Promise<AccountModel> {
-    this.encrypter.encrypt(account.password)
+    await this.encrypter.encrypt(account.password)
     return new Promise(resolve => resolve(null as unknown as AccountModel))
   }
 }
